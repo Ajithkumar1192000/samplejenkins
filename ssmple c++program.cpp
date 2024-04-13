b4d8dc4c02daa2634ec29f99cac73018bde7228d
Ajithkumar1192000 <118044395+Ajithkumar1192000@users.noreply.github.com>
2024-04-13 15:09:10 +0530

    Update ssmple c++program.cpp

--- ssmple c++program.cpp
+++ ssmple c++program.cpp
@@ -1,10 +1,12 @@
 #include<iostream.h>
 #include<stdio.h>
-void main(){
+void main()
+{
 cout<<"this is Ajithkumar"
 cout<<"this is C++ file"
 cout<<"this file will be added in git"
 cout<<"change this file"
 cout<<"changes made in file"
+cout<<"this is third changes"
 getch();
 }
